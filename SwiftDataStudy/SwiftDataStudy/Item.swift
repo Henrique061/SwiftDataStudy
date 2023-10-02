ce1aa762dce97ee802c8c1075f87064aea5176eb
Henrique Assis <henriquebatista061@gmail.com>
2023-10-02 13:55:14 -0300

    feat: created project

---
+++ SwiftDataStudy/SwiftDataStudy/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SwiftDataStudy
+//
+//  Created by Henrique Assis on 02/10/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SwiftDataStudy/SwiftDataStudy/SwiftDataStudyApp.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftDataStudyApp.swift
+//  SwiftDataStudy
+//
+//  Created by Henrique Assis on 02/10/23.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct SwiftDataStudyApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
